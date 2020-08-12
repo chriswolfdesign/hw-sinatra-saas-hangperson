835bdaff445f8907923858d8e141edb7fcf580ee
Chris <chriswolfdesign@gmail.com>
2020-08-12 10:40:44 -0400

    Implemented HangpersonGame.guess

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -19,6 +19,30 @@ class HangpersonGame
     @wrong_guesses = ''
   end
 
+  # @return [boolean] True if the letter was in the word, false otherwise
+  def guess(letter)
+
+    if not letter?(letter)
+      raise ArgumentError.new()
+    end
+
+    # convert the letter to lowercase
+    letter.downcase!
+
+    # if the letter has already been guesses, return false and quit
+    if @guesses.include? letter or @wrong_guesses.include? letter
+      return false
+    end
+
+    if @word.include? letter
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+
+    return true
+  end
+
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
@@ -31,4 +55,9 @@ class HangpersonGame
     }
   end
 
+end
+
+# @return [boolean] true if the given letter is a character, false otherwise
+def letter?(letter)
+  return letter =~ /[A-Za-z]/
 end
